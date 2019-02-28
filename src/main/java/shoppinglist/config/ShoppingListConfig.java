691e654c35c26343cb34eb21397916804b5fdd21
AndrejsVoitehovics <dumus@inbox.lv>
2019-02-28 15:20:34 +0200

    feature/7 The beginning of a big Spring journey

---
+++ src/main/java/shoppinglist/config/ShoppingListConfig.java
@@ -0,0 +1,10 @@
+package shoppinglist.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ShoppingList")
+public class ShoppingListConfig {
+
+}
